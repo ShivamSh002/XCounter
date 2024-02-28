04372ea558d7ff64cf8c071af962b2f85ea35428
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-28 22:23:37 +0530

    commit

--- x-counter/src/App.js
+++ x-counter/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     <div>
       <h1 className="Count_Value"> Counter App </h1> <p> Count: {count}</p>
       <button onClick={increment}> Increment </button>{" "}
-      <button onClick={decrement}> Decremen </button>{" "}
+      <button onClick={decrement}> Decrement </button>{" "}
     </div>
   );
 };
