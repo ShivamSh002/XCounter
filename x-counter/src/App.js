db63a092d692c5722381900ac89e064ef98ab91e
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-28 22:16:45 +0530

    commit

--- x-counter/src/App.js
+++ x-counter/src/App.js
@@ -1,25 +1,23 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    const increment = () => {
-        setCount(count + 1);
-    };
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-    const decrement = () => {
-        setCount(count - 1);
-    };
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
-    return ( <
-        div >
-        <
-        h1 className = "Count_Value" > Counter App < /h1> <p> Count : {count}</p >
-        <
-        button onClick = { increment } > Increment < /button>{" "} <
-        button onClick = { decrement } > Decremen < /button>{" "} <
-        /div>
-    );
+  return (
+    <div>
+      <h1 className="Count_Value"> Counter App </h1> <p> Count : {count}</p>
+      <button onClick={increment}> Increment </button>{" "}
+      <button onClick={decrement}> Decremen </button>{" "}
+    </div>
+  );
 };
 
 export default App;
